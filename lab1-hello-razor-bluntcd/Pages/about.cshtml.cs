1c89de6ca17832732874526fc988f707a5dc932e
ichanblunt <ichanblunt@gmail.com>
2025-09-06 10:08:34 -0400

    .cs files

---
+++ lab1-hello-razor-bluntcd/Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1_hello_razor_bluntcd.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ lab1-hello-razor-bluntcd/Pages/contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1_hello_razor_bluntcd.Pages
+{
+    public class contactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
